2f61a2a58c68f4fbbe1ce2e603f3434aea6e6c13
r.ivn <ishumakov881@gmail.com>
2025-08-18 20:17:30 +0300

    firts

--- app/src/main/java/com/mostbet/cricmost/MainActivity.kt
+++ app/src/main/java/com/mostbet/cricmost/MainActivity.kt
@@ -18,7 +18,12 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
